74370c6dc4f8b9a0743c76441bab5b3b4c5936b8
roalgon <145317171+roalgon@users.noreply.github.com>
2023-11-21 07:32:27 +0700

    Update hospital.cpp

--- hospital.cpp
+++ hospital.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
-#include <D:\Coding\header\hospital.h>
+#include "hospital.h"
 using namespace std;
 
 int main() {
@@ -33,45 +33,30 @@ int main() {
 
     billType bill(patientID, medicine);
 
-    cout << "**********************************" << endl;
-    cout << "********* Hospital Bill **********" << endl;
-    cout << "**********************************" << endl;
+    cout << "==================================" << endl;
+    cout << "========= Hospital Bill ==========" << endl;
+    cout << "==================================" << endl;
     cout << endl;
 
-    cout << "**********************************" << endl;
-    cout << "**********************************" << endl;
-    cout << "********* Doctor Details *********" << endl;
-    cout << "**********************************" << endl;
+    cout << "========= Doctor Details =========" << endl;
     cout << "   Dr. Name\t: " << patient.getDoctor().getName()  << endl; 
     cout << "   Specialty\t: " << patient.getDoctor().getSpeciality() << endl;
-    cout << "**********************************" << endl;
-    cout << "**********************************" << endl;
-    cout << endl;
 
-    cout << "**********************************" << endl;
-    cout << "**********************************" << endl;
-    cout << "********* Patient Details ********" << endl;
-    cout << "**********************************" << endl;
+    cout << "========= Patient Details ========" << endl;
     cout << "   Patient Name\t: " << patient.getName() << endl;
     cout << "   Patient ID\t: " << patient.getPatientID() << endl;
     cout << "   Age\t\t: " << patient.getAge() << endl;
     cout << "   Date Of Birth: ";
     patient.getBirth().printDate();
     cout << endl;
-    cout << "**********************************" << endl;
-    cout << "**********************************" << endl;
-    cout << endl;
 
-    cout << "**********************************" << endl;
-    cout << "**********************************" << endl;
-    cout << "********* Billing Details ********" << endl;
-    cout << "**********************************" << endl;
+    cout << "========= Billing Details ========" << endl;
     cout << "   Medicine Cost: $" << bill.getMedicine() << fixed << setprecision(0) << endl;
     cout << "   Doctor Fee\t: $" << bill.doctorFee() << fixed << setprecision(0) << endl;
     cout << "   Room Charges\t: $" << bill.roomCharges() << fixed << setprecision(0) << endl; 
     cout << "   Total pay\t: $" << bill.getMedicine() + bill.doctorFee() + bill.roomCharges() << fixed << setprecision(0) << endl;
-    cout << "**********************************" << endl;
-    cout << "**********************************" << endl;
+    cout << "==================================" << endl;
+    cout << endl;
 
     return 0;
 }
